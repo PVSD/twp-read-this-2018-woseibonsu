369794626aa59233d426733373ff8571219c1744
wo580 <winstonobonsu@gmail.com>
2018-11-05 11:20:12 -0500

    There is nothing to commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        File imports = new File("Imports.txt");
+        Scanner fileReader = new Scanner(imports);
+
     }
 }
